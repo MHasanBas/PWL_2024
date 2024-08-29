5bd16b0bda10992050c233c8ca404002cf161265
MHasanBas <hasanbasr054@gmail.com>
2024-08-29 10:11:24 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <TItle>Selamat Datang</TItle>
+    <head>
+        <h1 style="text-align: center;">Selamat Datang</h1>
+        <title>Kartu Tanda Penduduk</title>
+        <style>
+            .content {
+                justify-content: center;
+                align-items: center;
+                display: flex;
+                font-family: Inter, sans-serif;
+                margin-left: 5px;
+            }
+
+            table {
+                height: 550px;
+                padding: 1px;
+                width: 910px;
+                margin-top: 78px;
+                border: 0px solid;
+                border-radius: 1em;
+                background-color: #4DBEEF;
+                background-repeat: no-repeat;
+                background-size: cover;
+                padding-bottom: 15px;
+                padding-right: 10px;
+            }
+
+            .img {
+                padding-left: 10px;
+                height: 230px;
+                width: 170px;
+            }
+
+            td {
+                padding-left: 17px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="content">
+            <table style="font-size: 14.5pt;">
+                <tr>
+                    <th colspan="5" style="font-size: 20pt;">PROVINSI JAWA TIMUR</th>
+                </tr>
+                <tr>
+                    <th colspan="5" style="font-size: 20pt;">KABUPATEN NGANJUK</th>
+                </tr>
+                <tr>
+                    <td style="font-size: 21pt; font-weight: normal;">NIK </td>
+                    <td style="font-size: 21pt; font-weight: normal;">:  445454643333787900001</td>
+                </tr>
+                <tr>
+                    <td>Nama</td>
+                    <td>: M. HASAN BASRI</td>
+                    <td colspan="2"></td>
+                    <td align="center" rowspan="8"><img src="https://th.bing.com/th/id/OIP.F6Dl8_sjKUsHYmfp3zR2HAAAAA?rs=1&pid=ImgDetMain" class="img"></td>
+                </tr>
+                <tr>
+                    <td>Tempat/Tgl Lahir</td>
+                    <td>: NGANJUK, 01-12-2024</td>
+                </tr>
+                <tr>
+                    <td>Jenis Kelamin</td>
+                    <td>: LAKI-LAKI</td>
+                    <td>Gol. Darah :</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td>: SANGGRAHAN</td>
+                </tr>
+                <tr>
+                    <td style="padding-left: 55px;">RT/RW</td>
+                    <td>: 009/010</td>
+                </tr>
+                <tr>
+                    <td style="padding-left: 55px;">Kel/Desa</td>
+                    <td>: SANGGRAHAN</td>
+                </tr>
+                <tr>
+                    <td style="padding-left: 55px;">Kecamatan</td>
+                    <td>: PRAMBON</td>
+                </tr>
+                <tr>
+                    <td>Agama</td>
+                    <td>: ISLAM</td>
+                </tr>
+                <tr>
+                    <td>Status Perkawinan</td>
+                    <td>: BELUM KAWIN</td>
+                    <td colspan="2"></td>
+                    <td style="align-items: center; justify-content: center; display: flex;">NGANJUK</td>
+                </tr>
+                <tr>
+                    <td>Pekerjaan</td>
+                    <td>: PELAJAR/MAHASISWA</td>
+                    <td colspan="2"></td>
+                    <td style="align-items: center; justify-content: center; display: flex;">13-01-2025</td>
+                </tr>
+                <tr>
+                    <td>Kewarganegaraan</td>
+                    <td>: WNI</td>
+                    <td colspan="2"></td>
+                    <td align="center" rowspan="8"><img src="https://webkuliah.com/wp-content/uploads/2020/09/TTD-1.png" width="80" height="60"></td>
+                </tr>
+                <tr>
+                    <td>Berlaku Hingga</td>
+                    <td>: SEUMUR HIDUP</td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
